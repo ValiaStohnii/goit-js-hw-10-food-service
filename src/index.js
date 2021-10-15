ea017d3ca758602783014e3047363107dcc7ae29
ValiaStohnii <valia19961018@gmail.com>
2021-10-15 19:14:19 +0300

    add handlebars

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import menuTpl from './templates/menu.hbs'
+import menu from './menu.json';
 import './sass/main.scss';
+
+
+
+const menuList = document.querySelector('.js-menu');
+
+const cardsMenu = createMenu(menu);
+
+menuList.insertAdjacentHTML('beforeend', cardsMenu);
+
+function createMenu(menu) {
+  return menu.map(menuTpl).join(''); 
+}
