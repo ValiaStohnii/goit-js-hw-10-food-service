3c9a7156f5f4029d27788c1970678d3c58d446e3
ValiaStohnii <valia19961018@gmail.com>
2021-10-15 23:52:55 +0300

    change-theme

--- src/index.js
+++ src/index.js
@@ -13,3 +13,40 @@ menuList.insertAdjacentHTML('beforeend', cardsMenu);
 function createMenu(menu) {
   return menu.map(menuTpl).join(''); 
 }
+
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+    body: document.querySelector('body'),
+    themeSwitch: document.querySelector('#theme-switch-toggle'),
+}
+
+refs.themeSwitch.addEventListener('change', onThemeChange);
+
+themeDontChange();
+
+function onThemeChange() {
+    refs.body.classList.toggle(Theme.DARK);
+
+    if (refs.body.classList.contains(Theme.DARK)) {
+        localStorage.setItem('theme', Theme.DARK)
+        
+    } else { localStorage.setItem('theme', Theme.LIGHT) };
+
+}
+
+
+function themeDontChange() {
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme) {
+        refs.body.classList.add(savedTheme);
+    };
+
+    // refs.themeSwitch.checked = true;
+    
+}
